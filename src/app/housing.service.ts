d403236906d582539feb4ebcbbfe4e371a4d0e77
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2025-04-29 15:12:27 +0100

    feat: add application form and connect the data captured to the detail component

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -120,4 +120,10 @@ export class HousingService {
   }
 
   constructor() { }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}`,
+    );
+  }
 }
